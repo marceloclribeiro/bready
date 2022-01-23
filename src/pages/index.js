bf0898b43c3280d11a8321c7cdeb288c9e9be40f
marcelittle <mcribeiro@inf.ufpel.edu.br>
2022-01-23 00:00:09 -0300

    feat: starter components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import Layout from "../components/Layout"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Layout>This is my index</Layout>
 }
